69735ec2bbaa45a0519124c3177818de24ec38bd
Alaa Barka <tmakaveli643@gmail.com>
2023-02-11 15:02:01 +0100

    some basics of go programming language

---
+++ main.go
@@ -0,0 +1,21 @@
+package main 
+import "fmt math/rand" 
+
+
+
+func fill(n int, arr[n] int) {
+	for i:= ;i<n;i++{
+		arr[i] = rand.Intn(100) // min  max 100 or 99, idk 
+	}
+}
+
+func main () {
+	var n int ; 
+	fmt.Println("Enter the length of the array ! ") 
+	fmt.Scan(&n) 
+
+	var arr [n] int ; 
+	
+	
+
+}
