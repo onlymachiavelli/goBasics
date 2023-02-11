39aec7d11d0cc740417bfccca23bda1b5068e5be
Alaa Barka <tmakaveli643@gmail.com>
2023-02-11 15:36:34 +0100

    funcs and shits

--- main.go
+++ main.go
@@ -1,20 +1,43 @@
 package main 
-import "fmt math/rand" 
+import (
+	"fmt"
+	"math/rand"
+) 
 
 
 
-func fill(n int, arr[n] int) {
-	for i:= ;i<n;i++{
+func fill(n int, arr[5] int) {
+	for i:=0;i<n;i++ {
 		arr[i] = rand.Intn(100) // min  max 100 or 99, idk 
 	}
 }
 
-func main () {
-	var n int ; 
-	fmt.Println("Enter the length of the array ! ") 
-	fmt.Scan(&n) 
+func addShit(n int, arr[5] int) {
+	fmt.Println("Enter the number you want to add ! ")
+	var num int
+	fmt.Scan(&num) 
+	n++
 
-	var arr [n] int ; 
+	//push to the array 
+	//arr[n] = num
+	arr = append(array, num) 
+
+
+}
+
+func main () {
+	var n int=5 ; 
+	//fmt.Println("Enter the length of the array ! ") 
+	//fmt.Scan(&n) 
+
+	arr := make(int, 5) 
+
+	fill(n, &arr)
+
+	fmt.Println(arr)
+	addShit(n, &arr)
+	fmt.Println(arr)
+
 	
 	
 
